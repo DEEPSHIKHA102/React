44a65775804d409a53275030deac56b790385591
DeepShikha <paldeepshikha102@gmail.com>
2024-09-26 02:07:58 +0530

    initial commit

---
+++ deepshi-world/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <p>{5*5+5}</p>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.<br></br>
+          Now, Let's start Something new....
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Dshikha React
+        </a>
+      </header>
+    </div>
+    </>
+  );
+}
+
+export default App;
